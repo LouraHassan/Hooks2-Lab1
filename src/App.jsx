1be28983e21196d734747226129eaf293921c1b4
Lomooon <Alfarsi.loura@gmail.com>
2024-10-02 12:13:09 +0300

    col

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <div className='flex flex-col items-center p-5'>
 <p className='text-base-content text-4xl font-semibold'>Products list</p>
     
-      <div className='grid grid-cols-4 gap-10 place-content-center p-10'>
+      <div className='grid grid-cols-4 gap-10 place-content-center p-10 max-md:grid-cols-3 max-sm:grid-cols-2'>
 
       {items.map((item, index) => {
         return <div key={index} className=''>
